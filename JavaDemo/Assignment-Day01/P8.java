5b17be5b4b76af1b45ec22558c7a6fe2bb879e59
Mohd_Faizan_Khan <faizan10072002@gmail.com>
2024-07-30 16:33:43 +0530

    Commiting New Files

--- JavaDemo/Assignment-Day01/P8.java
+++ JavaDemo/Assignment-Day01/P8.java
@@ -1,12 +1,18 @@
 import java.util.*;
-public class P7{
+public class P8{
 	public static void main(String ...args){
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a 5 digit number: ");
 
+
+		int a = sc.nextInt();
 		
-		System.out.println("The sum of first and second last digit of the number is : ");
+
+		int b = a/10;
+		int c = b%10;
+		int d = b/1000;
+		System.out.println("The sum of first and second last digit of the number is : " + (c+d));
 		
 	}
 
